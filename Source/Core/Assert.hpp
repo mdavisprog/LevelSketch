76dfc9437d770614e5d8441826712ce42975f686
mdavisprog <mdavisprog@gmail.com>
2023-11-18 21:19:06 -0800

    Core: Assert function.

---
+++ Source/Core/Assert.hpp
@@ -0,0 +1,56 @@
+/**
+
+MIT License
+
+Copyright (c) 2023 Mitchell Davis <mdavisprog@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#pragma once
+
+#include "Types.hpp"
+
+#include <cstdio>
+
+namespace LevelSketch
+{
+namespace Core
+{
+
+template<typename... TArgs>
+static void Assertion(const char* File, u32 Line, bool Condition, const char* Format, const TArgs... Args)
+{
+    if (Condition)
+    {
+        return;
+    }
+
+    printf("%s", Format, &Args...);
+    printf("Assertion failed at %s:%u\n", File, Line);
+
+    std::abort();
+}
+
+}
+}
+
+#define LS_ASSERT(Condition) LevelSketch::Core::Assertion(__FILE__, __LINE__, Condition, #Condition)
+#define LS_ASSERTF(Condition, Format, ...) LevelSketch::Core::Assertion(__FILE__, __LINE__, Condition, Format, ##__VA_ARGS__)
