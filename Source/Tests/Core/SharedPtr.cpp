a230c92522a808c0963cb0a2f510cbdfebd17d9a
mdavisprog <mdavisprog@gmail.com>
2023-11-24 10:48:22 -0800

    Tests: Update SharedPtr tests.
    Make VERIFY statements only check a single value and
    add nullptr test cases.

--- Source/Tests/Core/SharedPtr.cpp
+++ Source/Tests/Core/SharedPtr.cpp
@@ -67,13 +67,26 @@ public:
 
 u32 Object::s_Counter { 0 };
 
+static bool CreateNull()
+{
+    Memory::SharedPtr<Object> Instance1 {};
+    VERIFY(Instance1.IsNull());
+    VERIFY(Instance1.GetReferenceCount() == 0);
+
+    Memory::SharedPtr<Object> Instance2 { nullptr };
+    VERIFY(Instance2.IsNull());
+    VERIFY(Instance2.GetReferenceCount() == 0);
+    return true;
+}
+
 static bool CreateDestroy()
 {
     Object::s_Counter = 0;
     VERIFY(Object::s_Counter == 0);
     {
         Memory::SharedPtr<Object> Object_ { Memory::SharedPtr<Object>::New() };
-        VERIFY(Object::s_Counter == 1 && Object_.GetReferenceCount() == 1);
+        VERIFY(Object::s_Counter == 1);
+        VERIFY(Object_.GetReferenceCount() == 1);
     }
     VERIFY(Object::s_Counter == 0);
     return true;
@@ -106,9 +119,12 @@ static bool ArrayPtrs()
     VERIFY(*Value == 1);
     Containers::Array<Memory::SharedPtr<int>> Values { Value, Value, Memory::SharedPtr<int>::New(5) };
     VERIFY(Value.GetReferenceCount() == 3);
-    VERIFY(*Values[0] == 1 && Values[0].GetReferenceCount() == 3);
-    VERIFY(*Values[1] == 1 && Values[1].GetReferenceCount() == 3);
-    VERIFY(*Values[2] == 5 && Values[2].GetReferenceCount() == 1);
+    VERIFY(*Values[0] == 1);
+    VERIFY(*Values[1] == 1);
+    VERIFY(*Values[2] == 5);
+    VERIFY(Values[0].GetReferenceCount() == 3);
+    VERIFY(Values[1].GetReferenceCount() == 3);
+    VERIFY(Values[2].GetReferenceCount() == 1);
     Values.Clear();
     VERIFY(Value.GetReferenceCount() == 1);
     return true;
@@ -117,9 +133,11 @@ static bool ArrayPtrs()
 static bool Move()
 {
     Memory::SharedPtr<int> Value { Memory::SharedPtr<int>::New(1) };
-    VERIFY(*Value == 1 && Value.GetReferenceCount() == 1);
+    VERIFY(*Value == 1);
+    VERIFY(Value.GetReferenceCount() == 1);
     Memory::SharedPtr<int> Moved { std::move(Value) };
-    VERIFY(*Moved == 1 && Moved.GetReferenceCount() == 1);
+    VERIFY(*Moved == 1);
+    VERIFY(Moved.GetReferenceCount() == 1);
     VERIFY(Value.IsNull());
     VERIFY(Value.GetReferenceCount() == 0);
     return true;
@@ -146,15 +164,31 @@ static bool SelfCopy()
     return true;
 }
 
+static bool SetNull()
+{
+    Memory::SharedPtr<Object> Value1;
+    VERIFY(Value1.GetReferenceCount() == 0);
+    Memory::SharedPtr<Object> Value2 { Memory::SharedPtr<Object>::New() };
+    VERIFY(Value2.GetReferenceCount() == 1);
+    Value1 = Value2;
+    VERIFY(Value1.GetReferenceCount() == 2);
+    Value1 = nullptr;
+    VERIFY(Value2.GetReferenceCount() == 1);
+    VERIFY(Value1.GetReferenceCount() == 0);
+    return true;
+}
+
 TestSuite* SharedPtr()
 {
     return new TestSuite("SharedPtr", {
+        TEST_CASE(CreateNull),
         TEST_CASE(CreateDestroy),
         TEST_CASE(CreateCopy),
         TEST_CASE(ArrayPtrs),
         TEST_CASE(Move),
         TEST_CASE(Equality),
-        TEST_CASE(SelfCopy)
+        TEST_CASE(SelfCopy),
+        TEST_CASE(SetNull)
     });
 }
 
