ec90399de7ef0c736c9f3840334016c68699fbb4
mdavisprog <mdavisprog@gmail.com>
2024-01-24 22:05:14 -0800

    Core: Various functions for Matrix4 operations.
    The operator* with another Matrix4 has been moved to outside
    the class to keep in-line with other operators.

--- Source/Core/Math/Matrix.hpp
+++ Source/Core/Math/Matrix.hpp
@@ -29,6 +29,7 @@ SOFTWARE.
 #include "../Types.hpp"
 #include "Math.hpp"
 #include "Rect.hpp"
+#include "Vector3.hpp"
 
 #include <initializer_list>
 
@@ -46,6 +47,21 @@ struct Matrix4
 public:
     static Matrix4<T> Identity;
     static Matrix4<T> Zero;
+
+    static Matrix4<T> LookAt(const Vector3<T>& Eye, const Vector3<T>& Center, const Vector3<T>& Up)
+    {
+        const Vector3<T> Forward { (Eye - Center).Normalize() };
+        const Vector3<T> Side { Forward.Cross(Up).Normalize() };
+        const Vector3<T> dUp { Side.Cross(Forward) };
+
+        return
+        {
+            Side.X, Side.Y, Side.Z, 0,
+            dUp.X, dUp.Y, dUp.Z, 0,
+            Forward.X, Forward.Y, Forward.Z, 0,
+            Eye.X, Eye.Y, Eye.Z, 1
+        };
+    }
 
     T Data[16];
 
@@ -100,30 +116,54 @@ public:
         };
     }
 
-    Matrix4<T> operator*(const Matrix4<T>& Other) const
+    Matrix4<T> RotateX(T Angle) const
     {
-        Matrix4<T> Result;
+        const T CosRad { Cos<T>(Angle * DEG2RAD) };
+        const T SinRad { Sin<T>(Angle * DEG2RAD) };
 
-        for (u32 Row = 0; Row < 4; Row++)
+        Matrix4<T> Rotation
         {
-            for (u32 Col = 0; Col < 4; Col++)
-            {
-                const u32 Destination { Row * 4 + Col };
-                Result[Destination] = 0.0f;
+            1, 0, 0, 0,
+            0, CosRad, -SinRad, 0,
+            0, SinRad, CosRad, 0,
+            0, 0, 0, 1
+        };
 
-                for (u32 I = 0; I < 4; I++)
-                {
-                    const u32 RowIndex { Row * 4 + I };
-                    const u32 ColIndex { Col + I * 4 };
-                    Result[Destination] += Data[RowIndex] * Other[ColIndex];
-                }
-            }
-        }
+        return *this * Rotation;
+    }
 
-        return Result;
+    Matrix4<T> RotateY(T Angle) const
+    {
+        const T CosRad { Cos<T>(Angle * DEG2RAD) };
+        const T SinRad { Sin<T>(Angle * DEG2RAD) };
+
+        Matrix4<T> Rotation
+        {
+            CosRad, 0, SinRad, 0,
+            0, 1, 0, 0,
+            -SinRad, 0, CosRad, 0,
+            0, 0, 0, 1
+        };
+
+        return *this * Rotation;
+    }
+
+    Matrix4<T> RotateZ(T Angle) const
+    {
+        const T CosRad { Cos<T>(Angle * DEG2RAD) };
+        const T SinRad { Sin<T>(Angle * DEG2RAD) };
+
+        Matrix4<T> Rotation
+        {
+            CosRad, -SinRad, 0, 0,
+            SinRad, CosRad, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        };
+
+        return *this * Rotation;
     }
 };
-
 
 template<typename T>
 Matrix4<T> Matrix4<T>::Identity
@@ -138,6 +178,56 @@ template<typename T>
 Matrix4<T> Matrix4<T>::Zero { 0 };
 
 typedef Matrix4<f32> Matrix4f;
+
+template<typename T>
+static inline Matrix4<T> operator*(const Matrix4<T>& A, T Scalar)
+{
+    Matrix4<T> Result {};
+
+    for (u32 I = 0; I < 16; I++)
+    {
+        Result[I] = A[I] * Scalar;
+    }
+
+    return Result;
+}
+
+template<typename T>
+static inline Matrix4<T> operator*(T Scalar, const Matrix4<T>& A)
+{
+    Matrix4<T> Result {};
+
+    for (u32 I = 0; I < 16; I++)
+    {
+        Result[I] = Scalar * A[I];
+    }
+
+    return Result;
+}
+
+template<typename T>
+static inline Matrix4<T> operator*(const Matrix4<T>& A, const Matrix4<T>& B)
+{
+    Matrix4<T> Result;
+
+    for (u32 Row = 0; Row < 4; Row++)
+    {
+        for (u32 Col = 0; Col < 4; Col++)
+        {
+            const u32 Destination { Row * 4 + Col };
+            Result[Destination] = 0.0f;
+
+            for (u32 I = 0; I < 4; I++)
+            {
+                const u32 RowIndex { Row * 4 + I };
+                const u32 ColIndex { Col + I * 4 };
+                Result[Destination] += A[RowIndex] * B[ColIndex];
+            }
+        }
+    }
+
+    return Result;
+}
 
 static inline Matrix4f PerspectiveMatrixRH(f32 FOVAngle, f32 Aspect, f32 Near, f32 Far)
 {
