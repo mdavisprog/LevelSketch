8980fcd04310174795d570bd2e1994fce57029a4
mdavisprog <mdavisprog@gmail.com>
2024-02-03 16:23:19 -0800

    Mac: Fill out stub Mouse functions.

--- Source/Platform/Mac/Mouse.mm
+++ Source/Platform/Mac/Mouse.mm
@@ -26,32 +26,62 @@ SOFTWARE.
 
 #include "../Mouse.hpp"
 #include "../../Core/Math/Vector2.hpp"
-#include "../Window.hpp"
+#include "Window.hpp"
+#include "WindowBridge.hpp"
+
+#import <AppKit/AppKit.h>
 
 namespace LevelSketch
 {
 namespace Platform
 {
 
+static bool g_CursorVisible { true };
+
 void Mouse::Show()
 {
+    if (!IsVisible())
+    {
+        [NSCursor unhide];
+        g_CursorVisible = true;
+    }
 }
 
 void Mouse::Hide()
 {
+    if (IsVisible())
+    {
+        [NSCursor hide];
+        g_CursorVisible = false;
+    }
 }
 
 bool Mouse::IsVisible()
 {
-    return true;
+    return g_CursorVisible;
 }
 
-void Mouse::SetPosition(const Vector2i&)
+void Mouse::SetPosition(const Vector2i& Position)
 {
+    CGEventSourceRef EventSource { CGEventSourceCreate(kCGEventSourceStateCombinedSessionState) };
+    CGEventSourceSetLocalEventsSuppressionInterval(EventSource, 0.0);
+    CGAssociateMouseAndMouseCursorPosition(0);
+    CGWarpMouseCursorPosition({ static_cast<f32>(Position.X), static_cast<f32>(Position.Y) });
+    CGAssociateMouseAndMouseCursorPosition(1);
+    CFRelease(EventSource);
 }
 
-void Mouse::SetPosition(Window*, const Vector2i&)
+void Mouse::SetPosition(Window* Target, const Vector2i& Position)
 {
+    @autoreleasepool
+    {
+        WindowBridge* Bridge { [WindowBridge Retrieve:Target->Handle()] };
+        NSPoint Point { static_cast<f32>(Position.X), static_cast<f32>(Position.Y) };
+        Point.y = Bridge.Window.contentView.frame.size.height - Point.y;
+        Point = [Bridge.Window convertPointToScreen:Point];
+        Point.y = Bridge.Window.screen.frame.size.height - Point.y;
+        SetPosition({ static_cast<i32>(Point.x), static_cast<i32>(Point.y) });
+    }
 }
 
 }
