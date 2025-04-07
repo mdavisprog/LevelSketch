72845c7ed81cda8c1f6f6bcd87e604567d1e1243
mdavisprog <mdavisprog@gmail.com>
2025-04-06 17:14:58 -0700

    Migrated root UI nodes to be its own componentt.

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,5 +1,4 @@
 use bevy::prelude::*;
-use bevy::render::camera::NormalizedRenderTarget;
 
 pub mod buttonex;
 pub mod droppable;
@@ -9,8 +8,7 @@ pub mod panels;
 pub mod style;
 
 mod sizer;
-
-use droppable::*;
+mod viewport;
 
 //
 // Public API
@@ -72,26 +70,8 @@ fn setup(
     mut commands: Commands,
     mut icons: ResMut<icons::Icons>,
 ) {
-    commands.spawn((
-        Node {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            ..default()
-        },
-        GlobalZIndex(-1),
-        PickingBehavior {
-            should_block_lower: false,
-            is_hoverable: true,
-        }
-    ))
-    .observe(on_root_over)
-    .observe(on_root_out)
-    .observe(on_root_down)
-    .observe(on_root_click)
-    .observe(on_root_drag_start)
-    .observe(on_root_drag_enter)
-    .observe(on_root_drag_over)
-    .observe(on_root_drag_drop);
+    let mut entity = commands.spawn(viewport::Viewport);
+    viewport::Viewport::observe(&mut entity);
 
     buttonex::ButtonEx::initialize(&mut commands);
 
@@ -103,115 +83,4 @@ fn on_icons_loaded(
     mut commands: Commands,
 ) {
     panels::Shapes::create(&mut commands, &resources, Vec2::new(50.0, 50.0));
-}
-
-fn on_root_over(
-    _: Trigger<Pointer<Over>>,
-    mut state: ResMut<State>,
-) {
-    state.is_interacting = false;
-}
-
-fn on_root_out(
-    _: Trigger<Pointer<Out>>,
-    mut state: ResMut<State>,
-) {
-    state.is_interacting = true;
-}
-
-fn on_root_down(
-    trigger: Trigger<Pointer<Down>>,
-    mut commands: Commands,
-    mut state: ResMut<State>,
-) {
-    close_menus(&mut commands);
-
-    if trigger.button != PointerButton::Secondary {
-        return;
-    }
-
-    state.did_drag = false;
-}
-
-fn on_root_click(
-    trigger: Trigger<Pointer<Click>>,
-    state: Res<State>,
-    windows: Query<&Window>,
-    resources: Res<panels::Resources>,
-    mut commands: Commands,
-) {
-    if trigger.button != PointerButton::Secondary {
-        return;
-    }
-
-    if !state.did_drag {
-        let window_entity = match trigger.pointer_location.target {
-            NormalizedRenderTarget::Window(value) => Some(value.entity()),
-            _ => None,
-        };
-
-        let Some(window_entity) = window_entity else {
-            return;
-        };
-
-        let Ok(window) = windows.get(window_entity) else {
-            return;
-        };
-
-        let Some(cursor_position) = window.cursor_position() else {
-            return;
-        };
-
-        panels::Shapes::create(&mut commands, &resources, cursor_position);
-    }
-}
-
-fn on_root_drag_start(
-    trigger: Trigger<Pointer<DragStart>>,
-    mut state: ResMut<State>,
-) {
-    if trigger.button != PointerButton::Secondary {
-        return;
-    }
-
-    state.did_drag = true;
-}
-
-fn on_root_drag_enter(
-    trigger: Trigger<Pointer<DragEnter>>,
-    droppables: Query<&Droppable>,
-    mut commands: Commands,
-    mut drop_info: ResMut<DropInfo>,
-) {
-    let Ok(droppable) = droppables.get(trigger.dragged) else {
-        return;
-    };
-
-    drop_info.begin(droppable, trigger, &mut commands);
-}
-
-fn on_root_drag_over(
-    trigger: Trigger<Pointer<DragOver>>,
-    droppables: Query<&Droppable>,
-    mut commands: Commands,
-    mut drop_info: ResMut<DropInfo>,
-) {
-    let Ok(droppable) = droppables.get(trigger.dragged) else {
-        return;
-    };
-
-    drop_info.drag(droppable, trigger, &mut commands);
-}
-
-fn on_root_drag_drop(
-    trigger: Trigger<Pointer<DragDrop>>,
-    droppables: Query<&Droppable>,
-    mut commands: Commands,
-    mut drop_info: ResMut<DropInfo>,
-) {
-    let Ok(droppable) = droppables.get(trigger.dropped) else {
-        return;
-    };
-
-    drop_info.end(droppable, trigger, &mut commands);
 }
---
+++ src/gui/viewport.rs
@@ -0,0 +1,155 @@
+use bevy::prelude::*;
+use bevy::render::camera::NormalizedRenderTarget;
+use super::droppable::{Droppable, DropInfo};
+use super::panels;
+use super::State;
+
+/// UI Node that rests on top of the world viewport and below all other GUI nodes.
+/// This node acts as a catch all for any interaction with viewport.
+#[derive(Component)]
+#[require(
+    Node(Self::node),
+    GlobalZIndex(|| GlobalZIndex(-1)),
+    PickingBehavior(Self::picking_behavior),
+)]
+pub struct Viewport;
+
+impl Viewport {
+    pub fn observe(commands: &mut EntityCommands) {
+        commands
+            .observe(Self::on_over)
+            .observe(Self::on_out)
+            .observe(Self::on_down)
+            .observe(Self::on_click)
+            .observe(Self::on_drag_start)
+            .observe(Self::on_drag_enter)
+            .observe(Self::on_drag_over)
+            .observe(Self::on_drag_drop);
+    }
+
+    fn on_over(
+        _: Trigger<Pointer<Over>>,
+        mut state: ResMut<State>,
+    ) {
+        state.is_interacting = false;
+    }
+    
+    fn on_out(
+        _: Trigger<Pointer<Out>>,
+        mut state: ResMut<State>,
+    ) {
+        state.is_interacting = true;
+    }
+    
+    fn on_down(
+        trigger: Trigger<Pointer<Down>>,
+        mut commands: Commands,
+        mut state: ResMut<State>,
+    ) {
+        super::close_menus(&mut commands);
+    
+        if trigger.button != PointerButton::Secondary {
+            return;
+        }
+    
+        state.did_drag = false;
+    }
+    
+    fn on_click(
+        trigger: Trigger<Pointer<Click>>,
+        state: Res<State>,
+        windows: Query<&Window>,
+        resources: Res<panels::Resources>,
+        mut commands: Commands,
+    ) {
+        if trigger.button != PointerButton::Secondary {
+            return;
+        }
+    
+        if !state.did_drag {
+            let window_entity = match trigger.pointer_location.target {
+                NormalizedRenderTarget::Window(value) => Some(value.entity()),
+                _ => None,
+            };
+    
+            let Some(window_entity) = window_entity else {
+                return;
+            };
+    
+            let Ok(window) = windows.get(window_entity) else {
+                return;
+            };
+    
+            let Some(cursor_position) = window.cursor_position() else {
+                return;
+            };
+    
+            panels::Shapes::create(&mut commands, &resources, cursor_position);
+        }
+    }
+    
+    fn on_drag_start(
+        trigger: Trigger<Pointer<DragStart>>,
+        mut state: ResMut<State>,
+    ) {
+        if trigger.button != PointerButton::Secondary {
+            return;
+        }
+    
+        state.did_drag = true;
+    }
+    
+    fn on_drag_enter(
+        trigger: Trigger<Pointer<DragEnter>>,
+        droppables: Query<&Droppable>,
+        mut commands: Commands,
+        mut drop_info: ResMut<DropInfo>,
+    ) {
+        let Ok(droppable) = droppables.get(trigger.dragged) else {
+            return;
+        };
+    
+        drop_info.begin(droppable, trigger, &mut commands);
+    }
+    
+    fn on_drag_over(
+        trigger: Trigger<Pointer<DragOver>>,
+        droppables: Query<&Droppable>,
+        mut commands: Commands,
+        mut drop_info: ResMut<DropInfo>,
+    ) {
+        let Ok(droppable) = droppables.get(trigger.dragged) else {
+            return;
+        };
+    
+        drop_info.drag(droppable, trigger, &mut commands);
+    }
+    
+    fn on_drag_drop(
+        trigger: Trigger<Pointer<DragDrop>>,
+        droppables: Query<&Droppable>,
+        mut commands: Commands,
+        mut drop_info: ResMut<DropInfo>,
+    ) {
+        let Ok(droppable) = droppables.get(trigger.dropped) else {
+            return;
+        };
+    
+        drop_info.end(droppable, trigger, &mut commands);
+    }
+
+    fn node() -> Node {
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            ..default()
+        }
+    }
+
+    fn picking_behavior() -> PickingBehavior {
+        PickingBehavior { 
+            should_block_lower: false,
+            is_hoverable: true,
+        }
+    }
+}
