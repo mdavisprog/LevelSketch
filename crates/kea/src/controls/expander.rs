38f737e690a909031f855972481476d66c75b598
mdavisprog <mdavisprog@gmail.com>
2025-07-13 11:44:07 -0700

    Animate the KeaExpander contents when clicked.
    The contents will be clipped and shrunk when being collapsed and
    will animate back to normal when being expanded.

--- crates/kea/src/controls/expander.rs
+++ crates/kea/src/controls/expander.rs
@@ -1,5 +1,10 @@
 use bevy::prelude::*;
 use crate::{
+    animation::{
+        KeaAnimation,
+        KeaAnimationClip,
+        KeaAnimationComplete,
+    },
     observers::KeaObservers,
     style,
 };
@@ -8,18 +13,26 @@ use super::{
     separator::KeaSeparator,
 };
 
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum KeaExpanderState {
+    Expanded,
+    Collapsed,
+}
+
 #[derive(Component)]
 #[require(
     Node = Self::node(),
 )]
 pub struct KeaExpander {
-    _private: (),
+    state: KeaExpanderState,
+    expanded_height: f32,
 }
 
 impl KeaExpander {
     pub fn bundle(label: &str, contents: impl Bundle) -> impl Bundle {(
         Self {
-            _private: (),
+            state: KeaExpanderState::Expanded,
+            expanded_height: 0.0,
         },
         children![
             (
@@ -29,20 +42,37 @@ impl KeaExpander {
                 KeaSeparator::horizontal(),
             ),
             (
+                // Clips the entities with contents when being collapsed.
                 Node {
                     width: Val::Percent(100.0),
                     height: Val::Percent(100.0),
-                    overflow: Overflow::scroll(),
-                    align_items: AlignItems::Start,
+                    overflow: Overflow::clip(),
                     ..default()
                 },
-                Contents,
                 children![(
-                    contents,
+                    Node {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        overflow: Overflow::scroll(),
+                        align_items: AlignItems::Start,
+                        ..default()
+                    },
+                    Contents,
+                    children![(
+                        contents,
+                    )],
                 )],
             ),
         ]
     )}
+
+    pub fn is_expanded(&self) -> bool {
+        self.state == KeaExpanderState::Expanded
+    }
+
+    pub fn is_collapsed(&self) -> bool {
+        self.state == KeaExpanderState::Collapsed
+    }
 
     fn node() -> Node {
         Node {
@@ -104,7 +134,18 @@ impl Header {
 }
 
 #[derive(Component)]
+#[require(
+    KeaObservers = Self::observers(),
+)]
 struct Contents;
+
+impl Contents {
+    fn observers() -> KeaObservers {
+        KeaObservers::new(vec![
+            Observer::new(on_contents_anim_complete),
+        ])
+    }
+}
 
 fn on_click_header(
     trigger: Trigger<Pointer<Click>>,
@@ -112,13 +153,21 @@ fn on_click_header(
     parents: Query<&Children>,
     contents: Query<&Contents>,
     images: Query<&ImageNode>,
-    mut transforms: Query<&mut Transform>,
-    mut nodes: Query<(&mut Node, &mut Visibility)>,
+    computed_nodes: Query<&ComputedNode>,
+    mut expanders: Query<&mut KeaExpander>,
+    mut nodes: Query<&mut Node>,
+    mut commands: Commands,
 ) {
     let Ok(child) = children.get(trigger.target()) else {
         return;
     };
 
+    // The parent should be the expander component.
+    let Ok(mut expander) = expanders.get_mut(child.parent()) else {
+        return;
+    };
+
+    // Find the entity that has the Contents component.
     let mut contents_entity = Entity::PLACEHOLDER;
     for child in parents.iter_descendants(child.parent()) {
         if contents.contains(child) {
@@ -127,34 +176,89 @@ fn on_click_header(
         }
     }
 
-    if contents_entity == Entity::PLACEHOLDER {
-        return;
-    }
-
-    let Ok((mut node, mut visibility)) = nodes.get_mut(contents_entity) else {
+    let Ok(computed_node) = computed_nodes.get(contents_entity) else {
         return;
     };
 
-    node.display = match node.display {
-        Display::Flex => Display::None,
-        _ => Display::Flex,
+    let node_height = computed_node.content_size.y;
+
+    // Update the expander state.
+    expander.state = match expander.state {
+        KeaExpanderState::Expanded => {
+            expander.expanded_height = node_height;
+            KeaExpanderState::Collapsed
+        },
+        KeaExpanderState::Collapsed => KeaExpanderState::Expanded,
     };
 
-    *visibility = match node.display {
-        Display::Flex => Visibility::Visible,
-        _ => Visibility::Hidden,
+    // If the expander is being collapsed, override the height value to be Val::Px so that
+    // there is no visual artifact caused if starting value is Val::Percent.
+    if let Ok(mut node) = nodes.get_mut(contents_entity) {
+        if expander.state == KeaExpanderState::Collapsed {
+            node.height = Val::Px(node_height);
+        }
     };
 
+    let (top, height) = match expander.state {
+        KeaExpanderState::Expanded => (Val::Px(0.0), Val::Px(expander.expanded_height)),
+        KeaExpanderState::Collapsed => (Val::Px(-node_height), Val::Px(0.0)),
+    };
+
+    // Animate the node to move up, shrink in height, and clip.
+    commands
+        .entity(contents_entity)
+        .insert(
+            KeaAnimation::new_with_clips(vec![
+                KeaAnimationClip::new::<Node>("top", top, 0.5),
+                KeaAnimationClip::new::<Node>("height", height, 0.5),
+            ]),
+        );
+
+    // Update the rotation of the expander icon.
     for child in parents.iter_descendants(trigger.target()) {
         if images.contains(child) {
-            let Ok(mut transform) = transforms.get_mut(child) else {
+            let rotation = match expander.state {
+                KeaExpanderState::Expanded => Quat::from_rotation_z(0.0),
+                KeaExpanderState::Collapsed => Quat::from_rotation_z(180_f32.to_radians()),
+            };
+
+            commands
+                .entity(child)
+                .insert(KeaAnimation::new_with_clips(vec![
+                    KeaAnimationClip::new::<Transform>("rotation", rotation, 0.5),
+                ]));
+
+            break;
+        }
+    }
+}
+
+fn on_contents_anim_complete(
+    trigger: Trigger<KeaAnimationComplete>,
+    expanders: Query<&KeaExpander>,
+    children: Query<&ChildOf>,
+    mut nodes: Query<&mut Node>,
+) {
+    let event = trigger.event();
+
+    // The height value when fully expanded should be a Val::Percent(100.0). Unfortunately,
+    // animating Val::Percent does not impact the computed node size so the height needs to
+    // set manually. When the header is fully expanded and the animation completes, this
+    // will restore the 'height' property to be Val::Percent(100.0).
+    if event.field == "height" {
+        let Ok(mut node) = nodes.get_mut(trigger.target()) else {
+            return;
+        };
+
+        // Find the expander component entity and get the expanded state.
+        for parent in children.iter_ancestors(trigger.target()) {
+            let Ok(expander) = expanders.get(parent) else {
                 continue;
             };
 
-            transform.rotation = match *visibility {
-                Visibility::Visible => Quat::from_rotation_z(0.0),
-                _ => Quat::from_rotation_z(180_f32.to_radians()),
-            };
+            if expander.state == KeaExpanderState::Expanded {
+                node.height = Val::Percent(100.0);
+            }
 
             break;
         }
