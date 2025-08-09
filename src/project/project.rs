1bfd366324f99f511a9f36fe3daed87ae9b58c08
mdavisprog <mdavisprog@gmail.com>
2025-08-09 11:03:51 -0700

    Retrieve all files when project is loaded.

--- src/project/project.rs
+++ src/project/project.rs
@@ -1,7 +1,10 @@
 use bevy::prelude::*;
 use std::{
     io,
-    path::Path,
+    path::{
+        Path,
+        PathBuf,
+    },
 };
 
 pub struct Project {
@@ -21,7 +24,69 @@ impl Project {
 
         self.folder = folder.to_string();
 
+        let files = self.find_source_files();
+        println!("Retrieved {} files.", files.len());
+
         Ok(())
+    }
+
+    fn find_source_files(&self) -> Vec<String> {
+        self.retrieve_files(&self.folder)
+    }
+
+    fn retrieve_files(&self, path: &str) -> Vec<String> {
+        let mut result = Vec::<String>::new();
+
+        let dirs = match std::fs::read_dir(path) {
+            Ok(result) => result,
+            Err(error) => {
+                println!("Failed to read directory '{path}'. Error: {error:?}");
+                return result;
+            }
+        };
+
+        for dir in dirs {
+            let entry = match dir {
+                Ok(result) => result,
+                Err(error) => {
+                    println!("Failed to read directory entry: {error:?}");
+                    continue;
+                }
+            };
+
+            let file_type = match entry.file_type() {
+                Ok(result) => result,
+                Err(error) => {
+                    println!("Failed to get file type for '{:?}'. Error: {error:?}", entry.file_name());
+                    continue;
+                }
+            };
+
+            let Ok(file_name) = entry.file_name().into_string() else {
+                println!("Failed to convert OsStr into string: {:?}!", entry.file_name());
+                continue;
+            };
+
+            let mut file_path = PathBuf::new();
+            file_path.set_file_name(path);
+            file_path.push(file_name);
+
+            let file_path_str = file_path
+                .as_os_str()
+                .to_str()
+                .unwrap_or_default();
+
+            if file_type.is_dir() {
+                let mut directory_files = self.retrieve_files(file_path_str);
+                result.append(&mut directory_files);
+            } else if file_type.is_file() {
+                result.push(file_path_str.to_string());
+            } else {
+                panic!("Symlink directories are not handled!");
+            }
+        }
+
+        result
     }
 }
 
