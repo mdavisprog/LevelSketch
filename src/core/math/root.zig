0a0a6f1ddc4893614fb38560f51b96244501302c
mdavisprog <mdavisprog@gmail.com>
2025-11-18 20:53:26 -0800

    Add 'Vec4' generic type with a Vec4f concrete type.

--- src/core/math/root.zig
+++ src/core/math/root.zig
@@ -3,6 +3,7 @@ const rect = @import("rect.zig");
 const std = @import("std");
 const vec2 = @import("vec2.zig");
 const vec3 = @import("vec3.zig");
+const vec4 = @import("vec4.zig");
 
 pub const Color4b = color.Color4(u8);
 pub const Color4f = color.Color4(f32);
@@ -18,6 +19,8 @@ pub const Vec2us = vec2.Vec2(usize);
 
 pub const Vec3i = vec3.Vec3(i32);
 pub const Vec3f = vec3.Vec3(f32);
+
+pub const Vec4f = vec4.Vec4(f32);
 
 test "refall" {
     std.testing.refAllDecls(@This());
---
+++ src/core/math/vec4.zig
@@ -0,0 +1,35 @@
+pub fn Vec4(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        pub const zero: Self = .{
+            .x = 0,
+            .y = 0,
+            .z = 0,
+            .w = 0,
+        };
+
+        x: T,
+        y: T,
+        z: T,
+        w: T,
+
+        pub fn init(x: T, y: T, z: T, w: T) Self {
+            return Self{
+                .x = x,
+                .y = y,
+                .z = z,
+                .w = w,
+            };
+        }
+
+        pub fn splat(value: T) Self {
+            return Self{
+                .x = value,
+                .y = value,
+                .z = value,
+                .w = value,
+            };
+        }
+    };
+}
