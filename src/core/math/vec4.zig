25f23597df467aa75ee518d6ea8944e96a056c53
mdavisprog <mdavisprog@gmail.com>
2025-12-04 11:39:01 -0800

    Add math operations for Vec4.

--- src/core/math/vec4.zig
+++ src/core/math/vec4.zig
@@ -38,5 +38,86 @@ pub fn Vec4(comptime T: type) type {
                 self.z == other.z and
                 self.w == other.w;
         }
+
+        pub fn toArray(self: Self) [4]T {
+            return .{ self.x, self.y, self.z, self.w };
+        }
+
+        pub fn add(self: Self, other: Self) Self {
+            return .{
+                .x = self.x + other.x,
+                .y = self.y + other.y,
+                .z = self.z + other.z,
+                .w = self.w + other.w,
+            };
+        }
+
+        pub fn addMut(self: *Self, other: Self) *Self {
+            self.x += other.x;
+            self.y += other.y;
+            self.z += other.z;
+            self.w += other.w;
+            return self;
+        }
+
+        pub fn sub(self: Self, other: Self) Self {
+            return .{
+                .x = self.x - other.x,
+                .y = self.y - other.y,
+                .z = self.z - other.z,
+                .w = self.w - other.w,
+            };
+        }
+
+        pub fn subMut(self: *Self, other: Self) *Self {
+            self.x -= other.x;
+            self.y -= other.y;
+            self.z -= other.z;
+            self.w -= other.w;
+            return self;
+        }
+
+        pub fn mul(self: Self, other: Self) Self {
+            return .{
+                .x = self.x * other.x,
+                .y = self.y * other.y,
+                .z = self.z * other.z,
+                .w = self.w * other.w,
+            };
+        }
+
+        pub fn mulScalar(self: Self, scalar: T) Self {
+            return .{
+                .x = self.x * scalar,
+                .y = self.y * scalar,
+                .z = self.z * scalar,
+                .w = self.w * scalar,
+            };
+        }
+
+        pub fn mulMut(self: *Self, other: Self) *Self {
+            self.x *= other.x;
+            self.y *= other.y;
+            self.z *= other.z;
+            self.w *= other.w;
+            return self;
+        }
+
+        pub fn div(self: Self, other: Self) Self {
+            return .{
+                .x = self.x / other.x,
+                .y = self.y / other.y,
+                .z = self.z / other.z,
+                .w = self.w / other.w,
+            };
+        }
+
+        pub fn divMut(self: *Self, other: Self) *Self {
+            self.x /= other.x;
+            self.y /= other.y;
+            self.z /= other.z;
+            self.w /= other.w;
+            return self;
+        }
     };
 }
