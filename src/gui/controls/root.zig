1b044c6f2b7f683a12045e8726829657de6e3763
mdavisprog <mdavisprog@gmail.com>
2026-01-09 07:24:04 -0800

    Add dummy control that just takes up space.

--- src/gui/controls/root.zig
+++ src/gui/controls/root.zig
@@ -1,3 +1,15 @@
+const clay = @import("clay");
+
 pub const buttons = @import("buttons.zig");
 pub const images = @import("images.zig");
 pub const panels = @import("panels.zig");
+
+pub fn dummy(width: f32, height: f32) void {
+    clay.openElement();
+    clay.configureOpenElement(.{
+        .layout = .{
+            .sizing = .fixed(width, height),
+        },
+    });
+    clay.closeElement();
+}
