b6b9b52cbe524d672a34c817eee7cdadb1bd9331
mdavisprog <mdavisprog@gmail.com>
2026-01-10 11:50:00 -0800

    Basic implementation of a HashSetUnmanaged.
    Uses a AutoHashMapUnmanaged container under the hood.

--- src/core/containers/root.zig
+++ src/core/containers/root.zig
@@ -1,7 +1,9 @@
 const buffers = @import("buffers.zig");
+const sets = @import("sets.zig");
 const std = @import("std");
 
 pub const Buffer2D = buffers.Buffer2D;
+pub const HashSetUnmanaged = sets.HashSetUnmanaged;
 
 test "refall" {
     std.testing.refAllDecls(@This());
---
+++ src/core/containers/sets.zig
@@ -0,0 +1,63 @@
+const std = @import("std");
+
+pub fn HashSetUnmanaged(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        pub const empty: Self = .{};
+
+        _data: std.AutoHashMapUnmanaged(T, void) = .empty,
+
+        pub fn deinit(self: *Self, allocator: std.mem.Allocator) void {
+            self._data.deinit(allocator);
+        }
+
+        pub fn contains(self: Self, key: T) bool {
+            return self._data.contains(key);
+        }
+
+        pub fn count(self: Self) u32 {
+            return self._data.count();
+        }
+
+        pub fn insert(self: *Self, allocator: std.mem.Allocator, key: T) !void {
+            try self._data.put(allocator, key, {});
+        }
+
+        pub fn remove(self: *Self, key: T) bool {
+            return self._data.remove(key);
+        }
+    };
+}
+
+test "insert" {
+    const allocator = std.testing.allocator;
+
+    var set = HashSetUnmanaged(u32).empty;
+    defer set.deinit(allocator);
+
+    try set.insert(allocator, 5);
+    try set.insert(allocator, 18);
+    try set.insert(allocator, 42);
+
+    try std.testing.expectEqual(3, set.count());
+}
+
+test "remove" {
+    const allocator = std.testing.allocator;
+
+    var set = HashSetUnmanaged(u32).empty;
+    defer set.deinit(allocator);
+
+    try set.insert(allocator, 5);
+    try set.insert(allocator, 18);
+    try set.insert(allocator, 42);
+
+    try std.testing.expectEqual(3, set.count());
+
+    try std.testing.expectEqual(true, set.remove(18));
+    try std.testing.expectEqual(2, set.count());
+
+    try std.testing.expectEqual(false, set.remove(1));
+    try std.testing.expectEqual(2, set.count());
+}
